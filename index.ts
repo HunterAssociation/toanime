11e0543a7ef5de355987d121d724d96edb41bd2b
Hunter Association <109690253+HunterAssociation@users.noreply.github.com>
2023-02-28 16:16:29 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -240,7 +240,6 @@ const qqRequest = async (imgBuffer: Buffer) => {
     let singleImg: Buffer | null = null;
 
     switch (config.mode) {
-        
         case 'DIFFERENT_DIMENSION_ME': {
             const data = await request({
                 busiId: 'different_dimension_me_img_entry',
@@ -612,7 +611,16 @@ const onPhotoReceived = async (ctx: Context, userId: number, photoId: string, re
             throw new Error(`Unable to send media, please try again: ${errorsMgs}`);
         }
 
-        
+        if (config.messages.bye && currentUserStatus.requestsCount === 1) {
+            try {
+                await ctx.reply(config.messages.bye, {
+                    disable_web_page_preview: true,
+                    parse_mode: 'MarkdownV2',
+                });
+            } catch (e) {
+                console.error('Unable to send byeMessage for ' + userId, (e as Error).toString());
+            }
+        }
     } catch (e) {
         console.log('Error has occurred for ' + userId);
         console.error(e);
